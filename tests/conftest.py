ce64731772a35169592d6a12579390b7d1d8d1f5
ManousogiannisM <manousogm@gmail.com>
2022-06-21 20:44:40 +0200

    add docstring description

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,5 +3,20 @@ from pyspark.sql import SparkSession
 
 @pytest.fixture(scope="session")
 def spark_session():
+    """
+    We use a fixture to instantiate
+    the spark session object which we will
+    use for all our test cases.
+
+    Scope is important to be set to "session"
+    so that the object remains alive for the whole
+    test execution session.
+
+    """
     spark = SparkSession.builder.master("local[*]").appName("unit_test").getOrCreate()
-    return spark
+    spark.sparkContext.setLogLevel("ERROR") 
+    # We can also set number of partitions to 1, and
+    # the number of executors to 1 for cosnuming less
+    # resources.
+    yield spark
+    spark.stop()
--- tests/test_calculate_prices.py
+++ tests/test_calculate_prices.py
@@ -3,11 +3,23 @@ from ny_taxi_prices.calculate_prices import get_price_and_distance_per_vendor, c
 from chispa.dataframe_comparer import assert_approx_df_equality, assert_df_equality
 from pandas.testing import assert_frame_equal
 from pyspark_test import assert_pyspark_df_equal
-import os
+from pyspark.sql import Row
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType
 
+# Different ways of defining a sample Dataframe
 
-@pytest.fixture(scope="session")
-def nyc_taxi_input_data(spark_session):
+@pytest.fixture(scope="module") 
+def nyc_taxi_input_data_anti_pattern(spark_session):
+    """
+    This is a fixture created to demonstrate 
+    how you can create a spark dataframe for testing purposes.
+    It is NOT the recommended way, since the column types are 
+    note defined. Please avoid this.
+
+    PS - You do not need a fixture to create a test dataframe,
+    unless you need to use it in multiple test cases.
+    
+    """
     return (spark_session.createDataFrame(
         [
             ('Vendor_A', 10.0, 5.0),
@@ -18,16 +30,94 @@ def nyc_taxi_input_data(spark_session):
             ('Vendor_C', 40.0, 10.0),
             ("Vendor_C",float("NaN"),None)
         ],
-        schema = "vendor_id: string, total_amount: double, trip_distance: double"
-        #['vendor_id', 'total_amount', 'trip_distance']
+        ['vendor_id', 'total_amount', 'trip_distance']
     ))
 
 
-# testing with collect()
-# testing with subtract()
-# testing with pandas dataframe
-# testing with chispa
-def test_get_price_and_distance_per_vendor(spark_session, nyc_taxi_input_data):
+
+@pytest.fixture(scope="module")
+def nyc_taxi_input_data_schema_with_struct_type(spark_session):
+    """
+    This is how we can create a test dataframe 
+    with predefined schema types. ie. vendor_id 
+    is a non-nullable string, total_amount is a
+    nullable double etc.
+    
+    """
+
+    schema = (StructType([ 
+        StructField("vendor_id",StringType(),False), 
+        StructField("total_amount",DoubleType(),True), 
+        StructField("trip_distance",DoubleType(),True), 
+    ]))
+
+    return (spark_session.createDataFrame(
+        [
+            ('Vendor_A', 10.0, 5.0),
+            ('Vendor_B', 15.0, 5.0),
+            ('Vendor_C', 20.0, 5.0),
+            ('Vendor_A', 8.0, 4.0),
+            ('Vendor_A', 20.0, 10.0),
+            ('Vendor_C', 40.0, 10.0),
+            ("Vendor_C",float("NaN"),None)
+        ],
+        schema=schema
+    ))
+
+
+@pytest.fixture(scope="session")
+def nyc_taxi_input_data_schema_as_string(spark_session):
+    """
+    Define the dataframe schema as a string, without using StructType, 
+    and StructField. This makes it easier to create and requires less
+    code, and is easier to read.
+
+    """
+    
+    return (spark_session.createDataFrame(
+        [
+            ('Vendor_A', 10.0, 5.0),
+            ('Vendor_B', 15.0, 5.0),
+            ('Vendor_C', 20.0, 5.0),
+            ('Vendor_A', 8.0, 4.0),
+            ('Vendor_A', 20.0, 10.0),
+            ('Vendor_C', 40.0, 10.0),
+            #("Vendor_C",float("NaN"),None)
+        ],
+        schema = "vendor_id: string, total_amount: double, trip_distance: double"
+    ))
+
+
+
+def test_get_price_and_distance_per_vendor(spark_session, nyc_taxi_input_data_schema_as_string):
+    """
+    This is a test case example that demonstrates different ways of comparing 
+    two Spark Dataframes. The expected Dataframe and the actual Dataframe that
+    is returned by your tested function. The following methods are used:
+
+    - collect():will fail if the 'double' type columns are not exactly the same. 
+                (i.e 16.66666 != 16.665)
+
+    - subtract(): same as above
+
+    - pyspark-test: does not support approximate equality either.
+
+    - pandas: We convert the (small) spark dfs to pandas dfs.
+              Pandas testing supports approximate matching.
+              Disanvantages: - can not compare schemas 100% 
+                            (nullable, no nullable etc).
+                             - NaN and None values are handled
+                              differently in Spark. In Pandas
+                              they are the same.
+    
+    - chispa: - approximate spark df comparison is supported
+              - schema comparison is supported 100%. 
+                (including nullable, non-nullable)
+              - very nice error messages pop up when
+                a test fails. 
+
+    """
+
     expected_sdf = (spark_session.createDataFrame(
         [
             ('Vendor_A', 12.676, 6.333),
@@ -35,24 +125,27 @@ def test_get_price_and_distance_per_vendor(spark_session, nyc_taxi_input_data):
             ('Vendor_C', 30.0, 7.5),
             #(None,None,None)
         ],
-        schema = "vendor_id: string, average_amount: double, average_distance:double "
+        schema = "vendor_id: string, average_amount: double, average_distance: double "
         #['vendor_id', 'average_amount', 'average_distance']
     ))
 
-    actual_sdf = get_price_and_distance_per_vendor(nyc_taxi_input_data)
-    #chispa
-    assert_approx_df_equality(actual_sdf,expected_sdf,precision=0.1, ignore_nullable=True)
-    #assert_df_equality(actual_sdf,expected_sdf)
+    actual_sdf = get_price_and_distance_per_vendor(nyc_taxi_input_data_schema_as_string)
 
-    # collect
+    # collect: 
     #assert actual_sdf.schema == expected_sdf.schema
     #assert sorted(actual_sdf.collect()) == sorted(expected_sdf.collect())
 
-    # subtract
+    # subtract: 
     #assert actual_sdf.subtract(expected_sdf).count() == 0
 
-    #pandas
+    # pyspark-test: 
+    #assert_pyspark_df_equal(actual_sdf,expected_sdf)
+
+    # pandas: 
     #assert_frame_equal(actual_sdf.toPandas(),expected_sdf.toPandas(),check_less_precise=1)
 
-    #pyspark-test: No support for approx equality
-    #assert_pyspark_df_equal(actual_sdf,expected_sdf)
+    # chispa: 
+    assert_approx_df_equality(actual_sdf,expected_sdf,precision=0.1)
+    #assert_df_equality(actual_sdf,expected_sdf)
+
+
